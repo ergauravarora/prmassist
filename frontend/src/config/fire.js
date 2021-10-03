313c78d7341f08b6a22d2d6b7e194f13bc04486d
Gaurav arora <agaurav546@gmail.com>
2021-10-03 16:52:57 +0530

    react changed to enviroment variable

--- frontend/src/config/fire.js
+++ frontend/src/config/fire.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase"
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDSjCIe7eOKItlGQKJLJvIZUtYH8jSQR3M",
-    authDomain: "prm-assist-portal.firebaseapp.com",
-    projectId: "prm-assist-portal",
-    storageBucket: "prm-assist-portal.appspot.com",
-    messagingSenderId: "11497641483",
-    appId: "1:11497641483:web:e747975203a733df6ad90e"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 try {
   firebase.initializeApp(firebaseConfig);
