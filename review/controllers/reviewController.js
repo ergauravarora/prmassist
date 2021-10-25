e0d526f5122de24f0067714e88fd2f10543da424
gaurav.a <gaurav.a@cisinlabs.com>
2021-10-25 14:20:11 +0530

    add review api done without keyword

--- review/controllers/reviewController.js
+++ review/controllers/reviewController.js
@@ -6,57 +6,56 @@ const utils = require('../review/index');
 
 const addReviewAssistance = async (req, res, next) => {
     try {
-        const response = await reviewCrud.create(req.body);
-        // var DepartureAirportsServiceReview = (
-        //     {...review.departure,
-        //         airport:review.departureAirport,
-        //         date:new Date(),
-        //         airportCode:airportCode
-        //     });
-        // var ArrivalAirportsServiceReview =  await addAirportsServiceReviewA(
-        //     {...review.arrival,
-        //         airport:review.arrivalAirport,
-        //         date:new Date(),
-        //         airportCode:airportCode
-        //     });
-        // var AirlineServiceReview =          await addAirlineServiceReview(
-        //     {...review.flight,
-        //         airline:review.airline,
-        //         date:new Date(),
-        //         airportCode:airportCode
-        //     });
-        // var PrmassistReview=                await addPrmassistReview(
-        //     {...review.prmassist,
-        //         bookingId:review.bookingId,
-        //         date:new Date(),
-        //         airportCode:airportCode
-        //     });
-        // if(ArrivalAirportsServiceReview.ok && DepartureAirportsServiceReview.ok && AirlineServiceReview.ok && PrmassistReview.ok)
-        // {
-        //     var reviewData = {...review};
-        //     delete reviewData['departure'];
-        //     delete reviewData['arrival'];
-        //     delete reviewData['flight'];
-        //     delete reviewData['prmassist'];
-        //     reviewData = {...reviewData,
-        //         departureReviewID:DepartureAirportsServiceReview.id,
-        //         arrivalReviewID:ArrivalAirportsServiceReview.id,
-        //         flightReviewID:AirlineServiceReview.id,
-        //         prmassistReviewID:PrmassistReview.id,
-        //         date:new Date(),
-        //         airportCode:airportCode
-        //     }
+        var review = req.body
+        var airportCode = req.query.code
+        
+        const DepartureAirportsServiceReview = await reviewCrud.airportsCreate({...review.departure,
+            airport:review.departureAirport,
+            date:new Date(),
+            airportCode:airportCode
+        });        
+        var ArrivalAirportsServiceReview =  await reviewCrud.airportsCreate(
+            {...review.arrival,
+                airport:review.arrivalAirport,
+                date:new Date(),
+                airportCode:airportCode
+            });
+        var AirlineServiceReview =          await reviewCrud.airlineCreate(
+            {...review.flight,
+                airline:review.airline,
+                date:new Date(),
+                airportCode:airportCode
+            });
+        var PrmassistReview=                await reviewCrud.prmassistCreate(
+            {...review.prmassist,
+                bookingId:review.bookingId,
+                date:new Date(),
+                airportCode:airportCode
+            });
+        if(ArrivalAirportsServiceReview.ok && DepartureAirportsServiceReview.ok && AirlineServiceReview.ok && PrmassistReview.ok)
+        {
+            var reviewData = {...review};
+            delete reviewData['departure'];
+            delete reviewData['arrival'];
+            delete reviewData['flight'];
+            delete reviewData['prmassist'];
+            reviewData = {...reviewData,
+                departureReviewID:DepartureAirportsServiceReview.id,
+                arrivalReviewID:ArrivalAirportsServiceReview.id,
+                flightReviewID:AirlineServiceReview.id,
+                prmassistReviewID:PrmassistReview.id,
+                date:new Date(),
+                airportCode:airportCode
+            }
             
-        //     var resp = await reviewCrud.insert(reviewData, review.bookingId);
-        //     if(resp.ok)
-        //     {
-        //         return "Review Added";
-        //     }
-            
-        // }
-        console.log(response);
-        // se
-        res.status(201).json({response, msg: `Saved successfully`});
+             var response = await reviewCrud.Create(reviewData, review.bookingId);
+            if(response.ok)
+            {
+                console.log(response);
+                res.status(201).json({response, msg: `Saved successfully`});
+            }        
+        }
+        res.status(400).json({response, msg: `Something is wrong..`});
     } catch (e) {
         console.log(e);
         res.status(500).json({ errors: [{ msg: e.message}]});
--- review/crud/crud-review.js
+++ review/crud/crud-review.js
@@ -3,6 +3,10 @@ const nano = require('nano')(process.env.COUCHDB_URL || 'http://admin:admin@127.
 
 var review = nano.use('review');
 var airports_service_review = nano.use('airports_service_review');
+var airline_service_review = nano.use('airline_service_review');
+var prmassist_review = nano.use('prmassist_review');
+var review_for_assistance = nano.use('review_for_assistance');
+
 
 const findAirportsServiceReview = (selector) => {
     return new Promise(async (resolve, reject) => {
@@ -17,7 +21,52 @@ const findAirportsServiceReview = (selector) => {
     });
 }
 
+const airportsCreate = (reviewData) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await airports_service_review.insert(reviewData);
+            resolve(response);  
+        } catch (e) {
+            console.log(e);
+            reject(e);
+        }
+    });
+};
 
+const airlineCreate= (reviewData) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await airline_service_review.insert(reviewData);
+            resolve(response);  
+        } catch (e) {
+            console.log(e);
+            reject(e);
+        }
+    });
+};
+
+const prmassistCreate= (reviewData) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await prmassist_review.insert(reviewData);
+            resolve(response);  
+        } catch (e) {
+            console.log(e);
+            reject(e);
+        }
+    });
+};
+const Create= (reviewData) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await review_for_assistance.insert(reviewData);
+            resolve(response);  
+        } catch (e) {
+            console.log(e);
+            reject(e);
+        }
+    });
+};
 const create = (reviewData) => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -65,5 +114,5 @@ const _delete = () => {
 };
 
 module.exports = {
-    create, read, update, _delete ,findAirportsServiceReview
+    create, read, update, _delete ,findAirportsServiceReview,airportsCreate,airlineCreate,prmassistCreate,Create
 };
