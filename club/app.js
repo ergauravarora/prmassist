b451e17a4666cdcff7e0eacffa058dc36e08e0e0
gaurav.a <gaurav.a@cisinlabs.com>
2021-10-28 18:37:33 +0530

    email pug template

--- club/app.js
+++ club/app.js
@@ -3,8 +3,10 @@ const app = express();
 const clubRouter =  require('./routes/club');
 const swaggerUI =  require('swagger-ui-express');
 const swaggerSpec = require('./swagger.json');
+var path = require('path');
 const pug = require('pug');
 
+app.use('/public', express.static(path.join(__dirname, 'assets')))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'pug')
@@ -32,6 +34,33 @@ app.get('/', (req, res) => {
     ] }
 
   res.render('index', pugObj);
+
+ // res.send("ok")
+});
+app.get('/email', (req, res) => {
+
+  var pugObj = {
+    "id": "PRM-BOOKING-452",
+    "userId": "USER1234",
+    "firstName": "Gaurav",
+    "secondName": "Arora",
+    "email": "gaurav.a@cisinlabs.com",
+    "contact":"+91 8223886899",
+    "flightConfirmationDetails": {
+    "origin": "DUB",
+    "destination": "DXB",
+    "carrierCode": "162",
+    "flightNumber": "EK",
+    "departureDate": "2021-10-05"
+    },
+    "status": "idle",
+    "originConfirmed": true,
+    "destinationConfirmed": true,
+    "assistance": [
+    "BLND","MAAS","Other Assistance"
+    ] }
+
+  res.render('email', pugObj);
 
  // res.send("ok")
 });
