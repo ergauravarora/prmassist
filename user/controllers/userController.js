0fdd310f5aa6a39628eb9ba8b817bed665744a57
gaurav.a <gaurav.a@cisinlabs.com>
2021-10-30 02:05:28 +0530

    swaggar updated

--- user/controllers/userController.js
+++ user/controllers/userController.js
@@ -28,5 +28,30 @@ const GetUser = async (req, res, next) => {
     }
 };
 
+const UpdateUser =  async (req, res, next) => {
+    try {
+        //console.log(JSON.stringify(.id))
+        var {id } = req.query;
+        const user = await userCrud.read(selector.byId(id));
+        if(user.length > 0)
+        {
+            var updatedUser = {...user[0],...req.body};
+            
+            var response= await userCrud.create(updatedUser);
+            
+            // se
+            res.status(201).json({response, msg: `User Data Updated`});
+        }
+        else
+        {
+            res.status(400).json({user, msg: `No Data Found for user id "${id}"`});
+        }
+        
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ errors: [{ msg: e.message}]});
+    }
+}
 
-module.exports = { RegisterUser,GetUser };
+
+module.exports = { RegisterUser,GetUser,UpdateUser };
--- user/routes/user.js
+++ user/routes/user.js
@@ -1,19 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const userController = require("../controllers/userController");
-const { body, validationResult } = require('express-validator');
+const { body, validationResult,query } = require('express-validator');
 
 
 
 router.post('/registerUser', [
-    // body('userId').isString().notEmpty().trim().escape(),
-    // body('fullName').isString().notEmpty().trim().escape(),
-    // body('email').isString().notEmpty().trim().escape(),
-    // body('phoneNumber').isString().notEmpty().trim().escape(),
-    // body('profilePic').isString().notEmpty().trim().escape(),
-    // body('start').isString().notEmpty().trim().escape(),
-    // body('end').isString().notEmpty().trim().escape(),
-    // body('photo').isString().notEmpty().trim().escape()
+    body('personalDetail').exists(), //must have at least one item,
+    body('personalDetail.fullName').isString().notEmpty().trim().escape(),
+    body('personalDetail.email').isString().notEmpty().trim().escape(),
+    body('personalDetail.phoneNumber').isString().notEmpty().trim().escape(),
+    body('payments').exists(),
+    body('payments.*.cardNumber').isString().notEmpty().trim().escape(),
+    body('payments.*.expiry').isString().notEmpty().trim().escape(),
+    body('payments.*.ccv').isString().notEmpty().trim().escape(),
+    body('payments.*.cardHolderName').isString().notEmpty().trim().escape(),
+    body('payments.*.postalCode').isString().notEmpty().trim().escape(),
+    body('travelCompanions').exists(),
+    body('travelCompanions.*.firstName').isString().notEmpty().trim().escape(),
+    body('travelCompanions.*.secondName').isString().notEmpty().trim().escape(),
+    body('travelCompanions.*.nickname').isString().notEmpty().trim().escape(),
+    body('travelCompanions.*.email').isString().notEmpty().trim().escape(),
+    body('travelCompanions.*.phoneNumber').isString().notEmpty().trim().escape(),
+    body('travelDocuments').exists(),
+    body('travelDocuments.*.fullName').isString().notEmpty().trim().escape(),
+    body('travelDocuments.*.passportNumber').isString().notEmpty().trim().escape(),
+    body('travelDocuments.*.expiryDate').isString().notEmpty().trim().escape(),
+    body('travelDocuments.*.countryOfBirth').isString().notEmpty().trim().escape(),
+    body('bookingHistory').exists(),
+    body('bookingHistory.*').isString().notEmpty().trim().escape(),
+    body('qrCode').exists(),
+    body('qrCode.*').isString().notEmpty().trim().escape(),
+
+    
 ], (req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
@@ -22,7 +41,46 @@ router.post('/registerUser', [
     next();
 }, userController.RegisterUser);
 
-router.get('/getUser', (req, res, next) => {
+router.put('/updateUser', [
+    query('id').isString().notEmpty().trim().escape(),
+    body('personalDetail').exists(), //must have at least one item,
+    body('personalDetail.fullName').isString().notEmpty().trim().escape(),
+    body('personalDetail.email').isString().notEmpty().trim().escape(),
+    body('personalDetail.phoneNumber').isString().notEmpty().trim().escape(),
+    body('payments').exists(),
+    body('payments.*.cardNumber').isString().notEmpty().trim().escape(),
+    body('payments.*.expiry').isString().notEmpty().trim().escape(),
+    body('payments.*.ccv').isString().notEmpty().trim().escape(),
+    body('payments.*.cardHolderName').isString().notEmpty().trim().escape(),
+    body('payments.*.postalCode').isString().notEmpty().trim().escape(),
+    body('travelCompanions').exists(),
+    body('travelCompanions.*.firstName').isString().notEmpty().trim().escape(),
+    body('travelCompanions.*.secondName').isString().notEmpty().trim().escape(),
+    body('travelCompanions.*.nickname').isString().notEmpty().trim().escape(),
+    body('travelCompanions.*.email').isString().notEmpty().trim().escape(),
+    body('travelCompanions.*.phoneNumber').isString().notEmpty().trim().escape(),
+    body('travelDocuments').exists(),
+    body('travelDocuments.*.fullName').isString().notEmpty().trim().escape(),
+    body('travelDocuments.*.passportNumber').isString().notEmpty().trim().escape(),
+    body('travelDocuments.*.expiryDate').isString().notEmpty().trim().escape(),
+    body('travelDocuments.*.countryOfBirth').isString().notEmpty().trim().escape(),
+    body('bookingHistory').exists(),
+    body('bookingHistory.*').isString().notEmpty().trim().escape(),
+    body('qrCode').exists(),
+    body('qrCode.*').isString().notEmpty().trim().escape(),
+
+    
+], (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+    next();
+}, userController.UpdateUser);
+
+router.get('/getUser',[
+    query('id').isString().notEmpty().trim().escape()
+], (req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
